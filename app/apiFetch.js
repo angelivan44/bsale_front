4bd43c4532795cd071894682dc35865e1117a6a5
Angel Huayas <angelhuayas@gmail.com>
2022-02-10 18:12:10 -0500

    change host to the backend

--- app/apiFetch.js
+++ app/apiFetch.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://immense-beach-55627.herokuapp.com";
 
 async function apiFetch(...args) {
   const response = await fetch(...args);
