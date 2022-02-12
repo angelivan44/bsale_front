80c06e54a78081049279ce9223244b21c1ea63d4
Angel Huayas <angelhuayas@gmail.com>
2022-02-11 19:06:39 -0500

    add icon in search input

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,12 @@ export default function Main(parentElement) {
         </select>
 
         <div class="flex gap-8 form-js">
+            <div class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
             <input type="text" value="${this.currentQuery}" name="input" class="input-js input border h-10 border-gray-400 appearance-none rounded w-full px-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600">
+            </div>
             <button class="icon button-js flex items-center justify-center w-full gap-2" name = "button">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
